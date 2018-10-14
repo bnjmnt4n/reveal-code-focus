141d4cc4787fb9dbcf96ffa23d4352e75382868e
Toby Allsopp <toby@mi6.gen.nz>
2018-10-14 23:23:50 +1300

    Handle multiple fragments being shown at once (#17).
    Reveal.js allows multiple fragments to be assigned the same
    `data-fragment-index`, meaning that they are shown simultaneously.
    Focus all lines mentioned in all fragments that are shown.

--- reveal-code-focus.js
+++ reveal-code-focus.js
@@ -8,22 +8,13 @@
     return;
   }
 
-  var currentSlide, currentFragments, scrollToFocused = true, prevSlideData = null;
+  var currentSlide, currentFragmentsList, scrollToFocused = true, prevSlideData = null;
 
   // Iterates through `array`, running `callback` for each `array` element.
   function forEach(array, callback) {
     var i = -1, length = array ? array.length : 0;
     while (++i < length) {
       callback(array[i]);
-    }
-  }
-
-  function indexOf(array, elem) {
-    var i = -1, length = array ? array.length : 0;
-    while (++i < length) {
-      if (array[i] === elem) {
-        return i;
-      }
     }
   }
 
@@ -90,15 +81,15 @@
     Reveal.addEventListener('slidechanged', updateCurrentSlide);
 
     Reveal.addEventListener('fragmentshown', function(e) {
-      focusFragment(e.fragment);
+      focusFragments(e.fragments);
     });
 
     // TODO: make this configurable.
-    // When a fragment is hidden, clear the current focused fragment,
-    // and focus on the previous fragment.
+    // When fragments are hidden, clear the current focused fragments,
+    // and focus on the previous fragments.
     Reveal.addEventListener('fragmenthidden', function(e) {
-      var index = indexOf(currentFragments, e.fragment);
-      focusFragment(currentFragments[index - 1]);
+      var index = e.fragment.getAttribute('data-fragment-index');
+      focusFragments(currentFragmentsList[index - 1]);
     });
 
     updateCurrentSlide(e);
@@ -106,12 +97,21 @@
 
   function updateCurrentSlide(e) {
     currentSlide = e.currentSlide;
-    currentFragments = currentSlide.getElementsByClassName('fragment');
+    currentFragmentsList = [];
+
+    forEach(currentSlide.getElementsByClassName('fragment'), function(fragment) {
+      var fragmentIndex = fragment.getAttribute('data-fragment-index');
+      (
+        currentFragmentsList[fragmentIndex] ||
+        (currentFragmentsList[fragmentIndex] = [])
+      ).push(fragment);
+    });
+
     clearPreviousFocus();
 
     // If moving back to a previous slide…
     if (
-      currentFragments.length &&
+      currentFragmentsList.length &&
       prevSlideData &&
       (
         prevSlideData.indexh > e.indexh ||
@@ -120,9 +120,11 @@
     ) {
       // …return to the last fragment and highlight the code.
       while (Reveal.nextFragment()) {}
-      var currentFragment = currentFragments[currentFragments.length - 1];
-      currentFragment.classList.add('current-fragment');
-      focusFragment(currentFragment);
+      var currentFragment = currentFragmentsList[currentFragmentsList.length - 1];
+      forEach(currentFragment, function(currentFragment) {
+        currentFragment.classList.add('current-fragment');
+      });
+      focusFragments(currentFragment);
     }
 
     // Update previous slide information.
@@ -139,81 +141,84 @@
     });
   }
 
-  function focusFragment(fragment) {
+  function focusFragments(fragments) {
     clearPreviousFocus();
-    if (!fragment) {
+    if (!fragments) {
       return;
     }
 
-    var lines = fragment.getAttribute('data-code-focus');
-    if (!lines) {
-      return;
-    }
-
-    var codeBlock = parseInt(fragment.getAttribute('data-code-block'));
-    if (isNaN(codeBlock)) {
-      codeBlock = 1;
-    }
-
-    var preElems = currentSlide.querySelectorAll('pre');
-    if (!preElems.length) {
-      return;
-    }
-
-    var pre = preElems[codeBlock - 1];
-    var code = pre.querySelectorAll('code .line');
-    if (!code.length) {
-      return;
-    }
-
-    var topLineNumber, bottomLineNumber;
-
-    forEach(lines.split(','), function(line) {
-      lines = line.split('-');
-      if (lines.length == 1) {
-        focusLine(lines[0]);
-      } else {
-        var i = lines[0] - 1, j = lines[1];
-
-        while (++i <= j) {
-          focusLine(i);
-        }
-      }
-    });
-
-    function focusLine(lineNumber) {
-      // Convert from 1-based index to 0-based index.
-      lineNumber -= 1;
-
-      var line = code[lineNumber];
-      if (!line) {
+    forEach(fragments, function(fragment) {
+      var lines = fragment.getAttribute('data-code-focus');
+      if (!lines) {
         return;
       }
 
-      line.classList.add('focus');
+      var codeBlock = parseInt(fragment.getAttribute('data-code-block'));
+      if (isNaN(codeBlock)) {
+        codeBlock = 1;
+      }
 
-      if (scrollToFocused) {
-        if (topLineNumber == null) {
-          topLineNumber = bottomLineNumber = lineNumber;
+      var preElems = currentSlide.querySelectorAll('pre');
+      if (!preElems.length) {
+        return;
+      }
+
+      var pre = preElems[codeBlock - 1];
+      var code = pre.querySelectorAll('code .line');
+      if (!code.length) {
+        return;
+      }
+
+      forEach(lines.split(','), function(line) {
+        lines = line.split('-');
+        if (lines.length == 1) {
+          focusLine(lines[0]);
         } else {
-          if (lineNumber < topLineNumber) {
-            topLineNumber = lineNumber;
+          var i = lines[0] - 1, j = lines[1];
+
+          while (++i <= j) {
+            focusLine(i);
           }
-          if (lineNumber > bottomLineNumber) {
-            bottomLineNumber = lineNumber;
+        }
+      });
+
+      var topLineNumber, bottomLineNumber;
+
+      function focusLine(lineNumber) {
+        // Convert from 1-based index to 0-based index.
+        lineNumber -= 1;
+
+        var line = code[lineNumber];
+        if (!line) {
+          return;
+        }
+
+        line.classList.add('focus');
+
+        if (scrollToFocused) {
+          if (topLineNumber == null) {
+            topLineNumber = bottomLineNumber = lineNumber;
+          } else {
+            if (lineNumber < topLineNumber) {
+              topLineNumber = lineNumber;
+            }
+            if (lineNumber > bottomLineNumber) {
+              bottomLineNumber = lineNumber;
+            }
           }
         }
       }
-    }
 
-    if (scrollToFocused && topLineNumber != null) {
-      var topLine =  code[topLineNumber];
-      var bottomLine = code[bottomLineNumber];
-      var codeParent = topLine.parentNode;
-      var scrollTop = topLine.offsetTop;
-      var scrollBottom = bottomLine.offsetTop + bottomLine.clientHeight;
-      codeParent.scrollTop = scrollTop - (codeParent.clientHeight - (scrollBottom - scrollTop)) / 2;
-    }
+      // TODO: avoid touching the DOM layout properties multiple times for each fragment
+      if (scrollToFocused && topLineNumber != null) {
+        var topLine =  code[topLineNumber];
+        var bottomLine = code[bottomLineNumber];
+        var codeParent = topLine.parentNode;
+        var scrollTop = topLine.offsetTop;
+        var scrollBottom = bottomLine.offsetTop + bottomLine.clientHeight;
+        codeParent.scrollTop = scrollTop - (codeParent.clientHeight - (scrollBottom - scrollTop)) / 2;
+      }
+    });
   }
 
   function RevealCodeFocus(options) {
